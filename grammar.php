4195b221fb64085e08a3d458d5566e063d5ddc7b
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2021-10-26 22:54:28 +0300

    Release 0.19.0-rc2

---
+++ grammar.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TS\Language;
+
+use FFI;
+use FFI\CData;
+
+function php(): CData
+{
+    static $ffi;
+
+    if (!$ffi) {
+        // https://clang.llvm.org/docs/CrossCompilation.html
+        $arch = php_uname('m');
+        $vendor = match (PHP_OS_FAMILY) {
+            'Linux', 'BSD' => 'linux',
+            'Darwin' => 'apple',
+            'Windows' => 'w64',
+        };
+        $abi = match (PHP_OS_FAMILY) {
+            'Linux', 'BSD' => 'gnu',
+            'Darwin' => 'darwin',
+            'Windows' => 'mingw32',
+        };
+
+        $bin = sprintf('%s-%s-%s.%s', $arch, $vendor, $abi, PHP_SHLIB_SUFFIX);
+
+        $ffi = FFI::cdef(
+            'typedef struct TSLanguage TSLanguage;'
+            . PHP_EOL
+            . 'TSLanguage *tree_sitter_php();',
+            __DIR__ . '/' . $bin,
+        );
+    }
+
+    return $ffi->tree_sitter_php();
+}
